85baa3cdb445a7d2a7edf1e83d4c53375e444399
Theodore Dubois <tblodt@icloud.com>
2025-05-12 21:11:29 -0700

    lipu li pana e nasin tawa noka lipu la o kepeken ala toki ilo pali

--- scripts/addSkipTarget.js
+++ scripts/addSkipTarget.js
@@ -8,12 +8,3 @@ for (button of skipbuttons){
 var skiptarget = document.getElementsByTagName("h1")[0];
 skiptarget.setAttribute('tabindex', '-1');
 skiptarget.setAttribute('id', 'skip-target-main');
-
-skiptarget = document.getElementsByTagName("footer")[0];
-skiptarget.setAttribute('tabindex', '-1');
-skiptarget.setAttribute('id', 'skip-target-footer');
-
-
-
-
-
